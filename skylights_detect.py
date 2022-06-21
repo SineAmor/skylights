ebf3fed96b0ab31402b0c5d8d66c7b87e9102a4d
SineAmor <69848268+SineAmor@users.noreply.github.com>
2022-06-20 21:41:32 -0400

    Add files via upload

--- skylights_detect.py
+++ skylights_detect.py
@@ -16,5 +16,11 @@ def object_detection_image():
     st.title('Object Detection for Images')
     st.subheader('''This object detection project takes in an image and''' \
                  ''''outputs the image with bounding boxes created around the objects in the image''')
+    file = st.file_uploader('Upload Image', type = ['jpg', 'png', 'jpeg'])
+    if file != None:
+        img1 = Image.open(file)
+        img2 = np.array(img1)
+        
+        st.image(img1, caption = 'Uploaded Image')
     
 object_detection_image()
