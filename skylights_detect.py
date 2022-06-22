19e8a54484956505b2a96d54364ad3297cb70924
SineAmor <pterelaing@gmail.com>
2022-06-22 00:04:24 -0400

    Update skylights_detect.py

--- skylights_detect.py
+++ skylights_detect.py
@@ -17,20 +17,21 @@ def object_detection_image():
     st.title('Object Detection for Images')
     st.subheader('''This object detection project takes in an image and''' \
                  ''''outputs the image with bounding boxes created around the objects in the image''')
-    #file = st.file_uploader('Upload Image', type = ['jpg', 'png', 'jpeg'])
-    #if file != None:
-    #    img1 = Image.open(file)
-    #    img2 = np.array(img1)
+    file = st.file_uploader('Upload Image', type = ['jpg', 'png', 'jpeg'])
+    if file != None:
+        img1 = Image.open(file)
+        img2 = np.array(img1)
         
-    #    st.image(img1, caption = 'Uploaded Image')
-    #    my_bar = st.progress(0)
-    #    conf_threshold = st.slider('Confidence', 0, 100, 50)
-    #    nms_threshold = st.slider('Threshold', 0, 100, 20)
-    #    whT = 320
-    #    class_names = ['skylight']
-    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config_and_weights')
-        #weights_path = os.listdir('config_and_weights/yolov4-custom_best.weights')
-        #weights = open(weights_path, 'rb')
-    st.write(config_path)
+        st.image(img1, caption = 'Uploaded Image')
+        my_bar = st.progress(0)
+        conf_threshold = st.slider('Confidence', 0, 100, 50)
+        nms_threshold = st.slider('Threshold', 0, 100, 20)
+        whT = 320
+        class_names = ['skylight']
+        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
+                                   'config_and_weights', 'yolov4-custom.cfg')
+        weights_path = os.listdir(os.path.dirname(os.path.abspath(__file__)), 
+                                   'config_and_weights', 'yolov4-custom_best.weights')
+    st.write(weights_path)
     
 object_detection_image()
