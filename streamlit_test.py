559d81b4b9a10e8aea9f77fe177104de7719c1d7
SineAmor <pterelaing@gmail.com>
2022-07-04 15:59:59 -0400

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -42,9 +42,9 @@ def download_file(file_path):
             progress_bar.empty()
 
 def object_detector_ui():
-    st.sidebar.markdown('# Model')
-    confidence_thresh = st.slider('Confidence Threshold', 0, 100, 50)
-    overlap_thresh = st.slider('Overlap Threshold', 0, 100, 30)
+    st.sidebar.markdown('# Model Settings')
+    confidence_thresh = st.sidebar.slider('Confidence Threshold', 0, 100, 50)
+    overlap_thresh = st.sidebar.slider('Overlap Threshold', 0, 100, 30)
     return confidence_thresh, overlap_thresh
 
 def yolo_v3(confidence, overlap):
