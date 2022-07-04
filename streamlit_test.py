9cd435d1eb4b576ef4881fe6005ab335921489ea
SineAmor <pterelaing@gmail.com>
2022-07-04 18:32:58 -0400

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -88,7 +88,7 @@ def yolo_v3(img_arr, user_confidence, user_overlap):
             x, y, w, h = box[0], box[1], box[2], box[3]
             cv2.rectangle(img_arr, (x,y), (x+w, y+h), (240, 54, 230), 2)
             cv2.putText(img_arr, f'{class_names[class_ids[i]]} {int(confidences[i]*100)}%', (x, y-10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (240, 0, 240), 2)
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (240, 0, 240), 2)
             
     blob = cv2.dnn.blobFromImage(img_arr, 1/255, (416, 416), [0,0,0], 1, crop = False)
     net.setInput(blob)
